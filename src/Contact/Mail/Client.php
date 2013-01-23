a12fdb9f283205c122b6d9df157b6e85358f0226
Keith Pincombe <pincombe@gmail.com>
2013-01-23 19:11:26 +0000

    fixed namespace take 2

--- src/Contact/Mail/Client.php
+++ src/Contact/Mail/Client.php
@@ -9,7 +9,7 @@ class ClientException extends \Exception {}
 class Client extends \Transmit\Client
 {
 
-	public function send(Message $message)
+	public function send(Mail $message)
 	{
 		$response = $this->_post(sprintf('/mail', json_encode($message)));
 		return json_decode($response);
