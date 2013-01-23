c10887aac3c685e6c7ed2026fe2eea087abafb6f
Keith Pincombe <pincombe@gmail.com>
2013-01-23 20:17:11 +0000

    added parsing functions

--- src/Contact/Mail/Address.php
+++ src/Contact/Mail/Address.php
@@ -16,4 +16,43 @@ class Address
 		$this->to = $to;
 		$this->reply_to = $reply_to;
 	}
+
+    public function getFrom()
+    {
+	    return self::parseAddressList($this->from);
+    }
+
+    public function getRecipients()
+    {
+	    return self::parseAddressList($this->recipients);
+    }
+
+    public function getReplyTo()
+    {
+	    return self::parseAddressList($this->reply_to);
+    }
+
+
+    public static function parseAddressList($address_list)
+    {
+		$emails = array();
+
+		if(preg_match_all('/\s*"?([^><,"]+)"?\s*((?:<[^><,]+>)?)\s*/', $address_list, $matches, PREG_SET_ORDER) > 0)
+		{
+		    foreach($matches as $m)
+		    {
+		        if(! empty($m[2]))
+		        {
+		            $emails[trim($m[2], '<>')] = $m[1];
+		        }
+		        else
+		        {
+		            $emails[$m[1]] = '';
+		        }
+		    }
+		}
+
+		return $emails;
+    }
+
 }
